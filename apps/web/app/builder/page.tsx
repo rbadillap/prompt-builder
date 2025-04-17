8bd811419a27ebe876dde7f2ee025ed50e00fa64
Ronny Badilla <rbadillap@gmail.com>
2025-04-16 23:16:07 -0600

    feat: landing page (#5)
    * feat: landing page
    
    * feat: lock the workflows that are in progress

---
+++ apps/web/app/builder/page.tsx
@@ -0,0 +1,65 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import { NavActions } from "@/components/nav-actions"
+import { Builder } from "@/components/builder"
+import { InputContainer } from "@/components/input-container"
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@workspace/ui/components/breadcrumb"
+import { Separator } from "@workspace/ui/components/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@workspace/ui/components/sidebar"
+import { CommandIcon } from "lucide-react"
+
+export default function Page() {
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "350px",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2">
+          <div className="flex items-center gap-1 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">Building Your AI</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Prompt</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+          <div className="ml-auto px-3">
+            <NavActions />
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 px-4 py-10">
+          <InputContainer />
+          <div className="mx-auto h-[calc(100vh-16rem)] w-full max-w-3xl rounded-xl bg-muted/50 overflow-hidden">
+            <Builder />
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
+}
